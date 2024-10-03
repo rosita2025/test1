8d23a24cb6fea6914574eaa948bdbfa26000d8a4
mustapharajaa <111634909+mustapharajaa@users.noreply.github.com>
2024-10-03 21:57:07 +0200

    Update ContactUs.jsx

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -5,8 +5,8 @@ const ContactUs = () => {
     return (
         <div className="contact-us-container">
             <h2>Contact Us</h2>
-            <p>Email: <a href="mailto:contact@neevaa.uk">contact@neevaa.uk</a></p>
-            <p>Phone: <a href="tel:0701413055">0701413055</a></p>
+            <p>Email: <a href="mailto:contact@neevaa.co.uk">contact@neevaa.co.uk</a></p>
+            <p>Phone: <a href="tel:0601413055">0601413055</a></p>
         </div>
     );
 };
